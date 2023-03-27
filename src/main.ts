1a7bac56fc55d540295e7e3caf3986978a5ca5dc
2ndline <bob.murrell@gmail.com>
2023-03-27 15:34:10 -0500

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,21 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+//This will tell typescript that window also contains ngRef property
+type windowExtended = Window &
+  typeof globalThis & {
+    ngRef: NgModuleRef<AppModule>;
+  };
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then((ref) => {
+    // Ensure Angular destroys itself on hot reloads.
+    const _window = window as windowExtended;
+    if (_window['ngRef']) {
+      _window['ngRef'].destroy();
+    }
+    _window['ngRef'] = ref;
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
-
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+    // Otherwise, log the boot error
+  })
+  .catch((err) => console.error(err));
